7ffaf2dc3c87bada167bd8c719e755b12d415f9d
Lilka1_1 <lil2606@list.ru>
2026-03-19 12:02:32 +0300

    Add files via upload

--- lab7.py
+++ lab7.py
@@ -4,7 +4,7 @@ import requests
 
 def city_weather():
     city_name = 'Paris'
-    key = '7e4f09b0c528f83e8a67a0338b9aedce'
+    key = ''
     response = requests.post(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}')
     result = json.loads(response.text)
     print(f"Погода в городе {city_name}:\nТемпература: {result['main']['temp']-273:.1f}\nВлажность: {result['main']['humidity']}\nДавление: {result['main']['pressure']}")
